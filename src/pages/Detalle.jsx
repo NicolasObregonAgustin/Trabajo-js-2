e371b4a120e0488a272181b6afe3aba36470d090
Nicolas Obregon <nicolasagustinobregon@gmail.com>
2026-03-26 19:21:25 -0300

    se agrego react

--- src/pages/Detalle.jsx
+++ src/pages/Detalle.jsx
@@ -5,25 +5,22 @@ import { useParams } from "react-router-dom";
             id: 1,
             nombre: "Laptop",
             precio: 1200,
-            ImageURL: "img/laptop.jpg"
+
         },
         {
             id: 2,
             nombre: "Celulares",
             precio: 1000,
-            ImageURL: "img/celulares.jpg"
         },
         {
             id: 3,
             nombre: "Tablet",
             precio: 800,
-            ImageURL: "img/tablet.jpg"
         },
         {
             id: 4,
             nombre: "Smartwatch",
             precio: 400,
-            ImageURL: "/img/smartwatch.jpg"
         }
     ];
 
